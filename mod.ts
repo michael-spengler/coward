0113eeb277e51d2e889a02d6d0355958819bd0e7
fox-cat <al@alast.ai>
2020-05-08 12:21:08 -0700

    📝 Small formatting fix
    ;_;

--- mod.ts
+++ mod.ts
@@ -1,16 +1,16 @@
 export { Client as Coward } from "./src/Client.ts";
 
-export { Channel } 				from "./src/classes/Channel.ts";
-export { DMChannel } 			from "./src/classes/DMChannel.ts";
-export { DMGroupChannel } 		from "./src/classes/DMGroupChannel.ts";
-export { Guild } 				from "./src/classes/Guild.ts";
-export { GuildChannel } 		from "./src/classes/GuildChannel.ts";
+export { Channel } from "./src/classes/Channel.ts";
+export { DMChannel } from "./src/classes/DMChannel.ts";
+export { DMGroupChannel } from "./src/classes/DMGroupChannel.ts";
+export { Guild } from "./src/classes/Guild.ts";
+export { GuildChannel } from "./src/classes/GuildChannel.ts";
 export { GuildChannelCategory } from "./src/classes/GuildChannelCategory.ts";
-export { GuildMember } 			from "./src/classes/GuildMember.ts";
-export { GuildNewsChannel } 	from "./src/classes/GuildNewsChannel.ts"; // TODO(fox-cat): remove due to redundancy
-export { GuildStoreChannel }	from "./src/classes/GuildStoreChannel.ts"; // TODO(fox-cat): see above; contemplate
-export { GuildTextChannel } 	from "./src/classes/GuildTextChannel.ts";
-export { GuildVoiceChannel } 	from "./src/classes/GuildVoiceChannel.ts";
-export { Member } 				from "./src/classes/Message.ts";
-export { Role } 				from "./src/classes/Role.ts";
-export { User } 				from "./src/classes/User.ts";
+export { GuildMember } from "./src/classes/GuildMember.ts";
+export { GuildNewsChannel } from "./src/classes/GuildNewsChannel.ts"; // TODO(fox-cat): remove due to redundancy
+export { GuildStoreChannel } from "./src/classes/GuildStoreChannel.ts"; // TODO(fox-cat): see above; contemplate
+export { GuildTextChannel } from "./src/classes/GuildTextChannel.ts";
+export { GuildVoiceChannel } from "./src/classes/GuildVoiceChannel.ts";
+export { Member } from "./src/classes/Message.ts";
+export { Role } from "./src/classes/Role.ts";
+export { User } from "./src/classes/User.ts";
