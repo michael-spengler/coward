1aae16e2289f9ea73161110fe2bfc0cb5fbcb058
userterminal <55012346+userterminal@users.noreply.github.com>
2020-05-17 13:49:35 -0400

    Adding extra functions to (some) channels
    Adding send functions to the GuildTextChannels GuildNewsChannel, and DMChannel objects.
    
    Adding delete function to the GuildTextChannel and GuildNewsChannel objects

--- src/classes/GuildNewsChannel.ts
+++ src/classes/GuildNewsChannel.ts
@@ -1,20 +1,31 @@
-import { Client } from "../Client.ts";
-import { Message } from "./Message.ts";
-import { Channel } from "./Channel.ts";
-import { GuildChannel } from "./GuildChannel.ts";
+import { Client } from "../Client.ts"
+import { Message } from "./Message.ts"
+import { Channel } from "./Channel.ts"
+import { GuildChannel } from "./GuildChannel.ts"
+import { Client } from "../Client.ts"
 
 /**
  * Class representing a news channel in a guild
  * @extends GuildChannel
  */
 export class GuildNewsChannel extends GuildChannel {
-	public topic: string;
-	public lastMessageID: string; // TODO(fox-cat): contemplate message object here?
+	public topic: string
+	public lastMessageID: string // TODO(fox-cat): contemplate message object here?
+	protected _client: Client
 
 	constructor(data: any, client: Client) {
-		super(data, client);
+		super(data, client)
 
-		this.topic = data.topic || null;
-		this.lastMessageID = data.last_message_id || null;
+		this._client = client
+		this.topic = data.topic || null
+		this.lastMessageID = data.last_message_id || null
+	}
+
+	send(content: string) {
+		this._client.postMessage(this.id, content)
+	}
+
+	delete() {
+		this._client.deleteChannel(this.id)
 	}
 }
