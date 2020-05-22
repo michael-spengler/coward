500daa0823a8aac0b1b5f641263b1f9be2885b04
fox-cat <al@alast.ai>
2020-05-21 20:20:22 -0700

    (*^_^*) better ping-pong example

--- examples/ping-pong.ts
+++ examples/ping-pong.ts
@@ -1,12 +1,15 @@
-import { Client } from "../mod.ts";
+import { Client } from "https://deno.land/x/coward@dev/mod.ts";
 
 let client = new Client("TOKEN");
 
 const evt = client.evt;
 
+evt.ready.attach(() => console.log("READY!"))
+
 evt.messageCreate.attach(async ({ message }) => {
-	if (message.author.bot) return;
-	client.postMessage(message.channel.id, "received message");
+	if(message.content == "!ping") {
+		client.postMessage(message.channel.id, "pong!");
+	}
 })
 
 client.connect();
