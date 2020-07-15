fb34f5d0578ceea59a950fdc61baa522bd0a5834
fox-cat <al@alast.ai>
2020-07-14 18:20:03 -0700

    ʕ·ᴥ·　ʔ update dependencies, prepare v0.3.1
    i really am an idiot

--- deps.ts
+++ deps.ts
@@ -3,15 +3,15 @@ export {
   connectWebSocket,
   isWebSocketCloseEvent,
   WebSocket,
-} from "https://deno.land/std@0.52.0/ws/mod.ts";
+} from "https://deno.land/std@0.61.0/ws/mod.ts";
 
 // deferred promises :)
-export * from "https://deno.land/std@0.52.0/async/deferred.ts";
+export * from "https://deno.land/std@0.61.0/async/deferred.ts";
 
 // colours
-export * from "https://deno.land/std@0.52.0/fmt/colors.ts";
+export * from "https://deno.land/std@0.61.0/fmt/colors.ts";
 
 // Evt 
-export { Evt } from "https://deno.land/x/evt@1.7.12/mod.ts";
+export { Evt } from "https://deno.land/x/evt@1.7.13/mod.ts";
 
 export { use } from "https://raw.githubusercontent.com/michaelolof/typescript-mix/master/src/index.ts";
