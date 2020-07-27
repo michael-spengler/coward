2c4ab3ad9c3c28e091816281fa5fcaa895974939
Alex Howe <mail@ahowe.dev>
2020-07-27 13:54:14 +0100

    Moved Permissions/UserFlags/MessageFlags to Constants.ts

--- src/util/MessageFlags.ts
+++ src/util/MessageFlags.ts
@@ -1,12 +1,5 @@
 import { BitField, BitFieldResolvable } from "./BitField.ts";
-
-export const MessageFlags = new Map<string, number>([
-	["CROSSPOSTED", 1 << 0],
-	["IS_CROSSPOST", 1 << 1],
-	["SUPPRESS_EMBEDS", 1 << 2],
-	["SOURCE_MESSAGE_DELETED", 1 << 3],
-	["URGENT", 1 << 4]
-])
+import { MessageFlags } from "./Constants.ts";
 
 type MessageFlagType =
 	"CROSSPOSTED" |
