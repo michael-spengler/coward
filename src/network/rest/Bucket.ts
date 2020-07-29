effffb405fcb7285440ecaeda7e23c25cdc94840
MikuroXina <ryosukadnak@gmail.com>
2020-07-29 23:56:12 +0900

    :pencil2: Replace spaces to tabs

--- src/network/rest/Bucket.ts
+++ src/network/rest/Bucket.ts
@@ -1,78 +1,78 @@
 const pause = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export class Bucket {
-  private readonly queue: Array<Function> = [];
-  private isGlobal = false;
-  private globalResetTimeout = 5000;
-  private limit = 5;
-  private remaining = 1;
-  private resetTimeout = 5000;
+	private readonly queue: Array<Function> = [];
+	private isGlobal = false;
+	private globalResetTimeout = 5000;
+	private limit = 5;
+	private remaining = 1;
+	private resetTimeout = 5000;
 
-  private enableGlobal(date: number, retryAfter: number) {
-    this.isGlobal = true;
-    const offset = date - Date.now();
-    this.globalResetTimeout = retryAfter * 1000 -
-      (Date.now() + offset);
-  }
+	private enableGlobal(date: number, retryAfter: number) {
+		this.isGlobal = true;
+		const offset = date - Date.now();
+		this.globalResetTimeout = retryAfter * 1000 -
+			(Date.now() + offset);
+	}
 
-  private setLimit(limit: number) {
-    this.limit = limit;
-  }
+	private setLimit(limit: number) {
+		this.limit = limit;
+	}
 
-  private setRemaining(remaining: number | null) {
-    this.remaining = Math.max(remaining ?? 1, 1);
-  }
+	private setRemaining(remaining: number | null) {
+		this.remaining = Math.max(remaining ?? 1, 1);
+	}
 
-  private setResetTimeout(date: number, resetTimeout: number) {
-    const offset = date - Date.now();
-    this.resetTimeout = resetTimeout * 1000 -
-      (Date.now() + offset);
-  }
+	private setResetTimeout(date: number, resetTimeout: number) {
+		const offset = date - Date.now();
+		this.resetTimeout = resetTimeout * 1000 -
+			(Date.now() + offset);
+	}
 
-  public applyHeadersToBucket(headers: Headers): void {
-    if (headers.has("x-ratelimit-global")) {
-      this.enableGlobal(
-        Date.parse(headers.get("date")!),
-        +headers.get("retry-after")!,
-      );
-    }
+	public applyHeadersToBucket(headers: Headers): void {
+		if (headers.has("x-ratelimit-global")) {
+			this.enableGlobal(
+				Date.parse(headers.get("date")!),
+				+headers.get("retry-after")!,
+			);
+		}
 
-    if (headers.has("x-ratelimit-limit")) {
-      this.setLimit(+headers.get("x-ratelimit-limit")!);
-    }
+		if (headers.has("x-ratelimit-limit")) {
+			this.setLimit(+headers.get("x-ratelimit-limit")!);
+		}
 
-    const remaining = headers.get("x-ratelimit-remaining");
-    this.setRemaining(
-      remaining == null ? null : +remaining,
-    );
+		const remaining = headers.get("x-ratelimit-remaining");
+		this.setRemaining(
+			remaining == null ? null : +remaining,
+		);
 
-    if (headers.has("x-ratelimit-reset")) {
-      this.setResetTimeout(
-        Date.parse(headers.get("date")!),
-        +headers.get("x-ratelimit-reset")!,
-      );
-    }
-  }
+		if (headers.has("x-ratelimit-reset")) {
+			this.setResetTimeout(
+				Date.parse(headers.get("date")!),
+				+headers.get("x-ratelimit-reset")!,
+			);
+		}
+	}
 
-  public addQueue(func: Function): Promise<unknown> {
-    return new Promise((resolve) => {
-      this.queue.push(() => resolve(func(this)));
-      this.checkQueue();
-    });
-  }
+	public addQueue(func: Function): Promise<unknown> {
+		return new Promise((resolve) => {
+			this.queue.push(() => resolve(func(this)));
+			this.checkQueue();
+		});
+	}
 
-  public async checkQueue(): Promise<void> {
-    if (this.isGlobal) {
-      await pause(this.globalResetTimeout);
-      this.remaining = this.limit;
-    }
-    if (this.remaining === 0) {
-      await pause(this.resetTimeout);
-      this.remaining = this.limit;
-    }
-    if (this.queue.length > 0 && this.remaining !== 0) {
-      const head = this.queue.splice(0, 1)[0];
-      head();
-    }
-  }
+	public async checkQueue(): Promise<void> {
+		if (this.isGlobal) {
+			await pause(this.globalResetTimeout);
+			this.remaining = this.limit;
+		}
+		if (this.remaining === 0) {
+			await pause(this.resetTimeout);
+			this.remaining = this.limit;
+		}
+		if (this.queue.length > 0 && this.remaining !== 0) {
+			const head = this.queue.splice(0, 1)[0];
+			head();
+		}
+	}
 }
--- src/network/rest/RequestHandler.ts
+++ src/network/rest/RequestHandler.ts
@@ -2,60 +2,60 @@ import { Versions, Discord } from "../../util/Constants.ts";
 import { Bucket } from "./Bucket.ts";
 
 export class RequestHandler {
-  private static readonly userAgent =
-    `DiscordBot (https://github.com/fox-cat/coward), ${Versions.THIS}`;
-  private readonly rateLimitBuckets = new Map<string, Bucket>();
-  public global = false;
-  public globalReset = 0;
+	private static readonly userAgent =
+		`DiscordBot (https://github.com/fox-cat/coward), ${Versions.THIS}`;
+	private readonly rateLimitBuckets = new Map<string, Bucket>();
+	public global = false;
+	public globalReset = 0;
 
-  constructor(private readonly botToken: string) {}
+	constructor(private readonly botToken: string) {}
 
-  private addQueue(
-    func: Function,
-    method: string,
-    url: string,
-  ): Promise<unknown> {
-    const route = routify(method, url);
-    let bucket = this.rateLimitBuckets.get(route);
-    if (!bucket) {
-      bucket = new Bucket();
-      this.rateLimitBuckets.set(route, bucket);
-    }
-    return bucket.addQueue(func);
-  }
+	private addQueue(
+		func: Function,
+		method: string,
+		url: string,
+	): Promise<unknown> {
+		const route = routify(method, url);
+		let bucket = this.rateLimitBuckets.get(route);
+		if (!bucket) {
+			bucket = new Bucket();
+			this.rateLimitBuckets.set(route, bucket);
+		}
+		return bucket.addQueue(func);
+	}
 
-  public request(
-    method: string,
-    url: string,
-    data?: string | {
-      file?: {
-        file: File | Blob;
-        name: string;
-      };
-      [key: string]: any;
-    },
-  ): Promise<unknown> {
-    const headers: { [k: string]: string } = {
-      "User-Agent": RequestHandler.userAgent,
-      "Authorization": "Bot " + this.botToken,
-      "X-Ratelimit-Precision": "millisecond",
-    };
+	public request(
+		method: string,
+		url: string,
+		data?: string | {
+			file?: {
+				file: File | Blob;
+				name: string;
+			};
+			[key: string]: any;
+		},
+	): Promise<unknown> {
+		const headers: { [k: string]: string } = {
+			"User-Agent": RequestHandler.userAgent,
+			"Authorization": "Bot " + this.botToken,
+			"X-Ratelimit-Precision": "millisecond",
+		};
 
-    const body = typeof data === "string"
-      ? JSON.stringify({ content: data })
-      : this.makeBody({ ...data }, headers) ?? undefined;
+		const body = typeof data === "string"
+			? JSON.stringify({ content: data })
+			: this.makeBody({ ...data }, headers) ?? undefined;
 
-    const task = async (bucket: Bucket) =>
-      this.requestWithAttempts(
-        { url, init: { method, headers, body }, bucket },
-      );
-    return this.addQueue(
-      task,
-      method,
-      url,
-    );
+		const task = async (bucket: Bucket) =>
+			this.requestWithAttempts(
+				{ url, init: { method, headers, body }, bucket },
+			);
+		return this.addQueue(
+			task,
+			method,
+			url,
+		);
 
-    /** switch(response.status) {
+		/** switch(response.status) {
 			case 400: case 401: case 403: case 404: case 405: case 429:
 			case 502: case 500: case 503: case 504: case 507: case 508:
 				const json = await response.json();
@@ -72,73 +72,73 @@ export class RequestHandler {
 				break;
 		
 		}*/
-  }
+	}
 
-  private makeBody(
-    data: {
-      readonly [key: string]: unknown;
-      file?: { file: File | Blob; name: string } | undefined;
-    } | undefined,
-    headers: { [k: string]: string },
-  ): FormData | string | null {
-    if (data == null) {
-      return null;
-    }
-    if (data.file) {
-      const form = new FormData();
+	private makeBody(
+		data: {
+			readonly [key: string]: unknown;
+			file?: { file: File | Blob; name: string } | undefined;
+		} | undefined,
+		headers: { [k: string]: string },
+	): FormData | string | null {
+		if (data == null) {
+			return null;
+		}
+		if (data.file) {
+			const form = new FormData();
 
-      form.append("file", data.file.file, data.file.name);
-      // console.log(data.file.file["Symbol"]);
-      delete data.file;
-      if (0 < Object.keys(data).length) {
-        form.append("payload_json", new Blob([JSON.stringify(data)]));
-      }
-      return form;
-    }
-    headers["Content-Type"] = "application/json";
-    return JSON.stringify(data);
-  }
+			form.append("file", data.file.file, data.file.name);
+			// console.log(data.file.file["Symbol"]);
+			delete data.file;
+			if (0 < Object.keys(data).length) {
+				form.append("payload_json", new Blob([JSON.stringify(data)]));
+			}
+			return form;
+		}
+		headers["Content-Type"] = "application/json";
+		return JSON.stringify(data);
+	}
 
-  private async requestWithAttempts(
-    { url, init, bucket, attemptLimit = 3 }: {
-      readonly url: string;
-      readonly init?: RequestInit;
-      readonly bucket: Bucket;
-      readonly attemptLimit?: number;
-    },
-  ): Promise<unknown> {
-    for (let attempts = 0; attempts < attemptLimit; ++attempts) {
-      const response = await fetch(Discord.API + url, init);
-      if (response.status == 204) return;
+	private async requestWithAttempts(
+		{ url, init, bucket, attemptLimit = 3 }: {
+			readonly url: string;
+			readonly init?: RequestInit;
+			readonly bucket: Bucket;
+			readonly attemptLimit?: number;
+		},
+	): Promise<unknown> {
+		for (let attempts = 0; attempts < attemptLimit; ++attempts) {
+			const response = await fetch(Discord.API + url, init);
+			if (response.status == 204) return;
 
-      if (response.ok) {
-        bucket.applyHeadersToBucket(response.headers);
-        return await response.json();
-      }
-      if (response.status == 429) {
-        console.warn("Received a 429; :(");
-        bucket.applyHeadersToBucket(response.headers);
-      }
-      ++attempts;
-    }
-    throw new Error(`Failed after ${attemptLimit} attempts.`);
-  }
+			if (response.ok) {
+				bucket.applyHeadersToBucket(response.headers);
+				return await response.json();
+			}
+			if (response.status == 429) {
+				console.warn("Received a 429; :(");
+				bucket.applyHeadersToBucket(response.headers);
+			}
+			++attempts;
+		}
+		throw new Error(`Failed after ${attemptLimit} attempts.`);
+	}
 }
 
 const idReplacer = (substring: string, p: unknown): string =>
-  p === "channels" || p === "guilds" || p === "webhooks"
-    ? substring
-    : `/${p}/:id`;
+	p === "channels" || p === "guilds" || p === "webhooks"
+		? substring
+		: `/${p}/:id`;
 
 function routify(method: string, url: string): string {
-  const route = url
-    .replace(/\/([a-z-]+)\/(?:[0-9]{17,19})/g, idReplacer)
-    .replace(/\/reactions\/[^/]+/g, "/reactions/:id")
-    .replace(/^\/webhooks\/(\d+)\/[A-Za-z0-9-_]{64,}/, "/webhooks/$1/:token")
-    .replace(/\?.*$/, "");
+	const route = url
+		.replace(/\/([a-z-]+)\/(?:[0-9]{17,19})/g, idReplacer)
+		.replace(/\/reactions\/[^/]+/g, "/reactions/:id")
+		.replace(/^\/webhooks\/(\d+)\/[A-Za-z0-9-_]{64,}/, "/webhooks/$1/:token")
+		.replace(/\?.*$/, "");
 
-  if (method.toUpperCase() === "DELETE" && route.endsWith("/messages/:id")) { // Delete Messsage endpoint has its own ratelimit
-    return method + route;
-  }
-  return route;
+	if (method.toUpperCase() === "DELETE" && route.endsWith("/messages/:id")) { // Delete Messsage endpoint has its own ratelimit
+		return method + route;
+	}
+	return route;
 }
